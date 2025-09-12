7900cd74d544a3058b4dac75befb5b4e52a9d68d
Abhishek Singh <krabhisheke@gmail.com>
2025-09-13 01:59:45 +0530

    13 commit

--- src/main.jsx
+++ src/main.jsx
@@ -1,14 +1,17 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import './index.css'
-import AppWithRouter from './App.jsx'
-import { Analytics } from '@vercel/analytics/react'
-import { SpeedInsights } from '@vercel/speed-insights'
+import { StrictMode } from 'react';
+import { createRoot } from 'react-dom/client';
+import './index.css';
+import AppWithRouter from './App';
+import { Analytics } from '@vercel/analytics/react';
+import { SpeedInsights } from '@vercel/speed-insights/react';
 
-createRoot(document.getElementById('root')).render(
+const container = document.getElementById('root');
+const root = createRoot(container);
+
+root.render(
   <StrictMode>
     <AppWithRouter />
     <Analytics />
     <SpeedInsights />
   </StrictMode>
-)
+);
