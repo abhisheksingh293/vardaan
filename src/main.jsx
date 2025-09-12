a65b17b187931e9dc3ebc37e7c7f3bfc8d102934
Abhishek Singh <krabhisheke@gmail.com>
2025-09-13 01:43:35 +0530

    12 commit

--- src/main.jsx
+++ src/main.jsx
@@ -3,10 +3,12 @@ import { createRoot } from 'react-dom/client'
 import './index.css'
 import AppWithRouter from './App.jsx'
 import { Analytics } from '@vercel/analytics/react'
+import { SpeedInsights } from '@vercel/speed-insights'
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
     <AppWithRouter />
     <Analytics />
+    <SpeedInsights />
   </StrictMode>
 )
